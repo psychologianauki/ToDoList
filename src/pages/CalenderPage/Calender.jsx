b01841fd1af22ec987228fff937a95734c747fc0
Aleksy <psychologianauki@gmail.com>
2023-03-10 20:27:25 +0100

    created calendar

--- src/pages/CalenderPage/Calender.jsx
+++ src/pages/CalenderPage/Calender.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import styles from "./Calender.module.css";
-export default function Calender() {
-  return <div className={styles.calender}>Calender</div>;
+import Calendar from "react-calendar";
+
+import { useState } from "react";
+export default function calendar() {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div className="app">
+      <h1 className="text-center">React Calendar</h1>
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className="text-center">
+        <span className="bold">Selected Date:</span> {date.toDateString()}
+      </p>
+    </div>
+  );
 }
