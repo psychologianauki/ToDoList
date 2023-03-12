ad0f9ad50a2f6554d2fdcce150695573a35666f0
Aleksy <psychologianauki@gmail.com>
2023-03-12 18:46:44 +0100

    firebase 1

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import styles from "./RegisterPage.module.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../config";
 export default function RegisterPage() {
   const [inputValue, setInputValue] = useState("");
   const [inputValue2, setInputValue2] = useState("");
+  const [email, setEmail] = useState("");
   const [notSamePasswords, setNotSamePasswords] = useState(true);
   const [isButtonClicked, setIsButtonClicked] = useState(false);
   const handleChange = (event) => {
@@ -12,11 +15,18 @@ export default function RegisterPage() {
   const handleChange2 = (event) => {
     setInputValue2(event.target.value);
   };
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.main}>
         <div className={styles.inputes}>
-          <input placeholder="email"></input>
+          <input
+            placeholder="email"
+            value={email}
+            onChange={handleEmail}
+          ></input>
           <input
             placeholder="password"
             value={inputValue}
@@ -34,11 +44,23 @@ export default function RegisterPage() {
               console.log(inputValue);
               console.log(inputValue2);
               setIsButtonClicked(true);
+
               {
                 inputValue !== inputValue2
                   ? setNotSamePasswords(false)
                   : setNotSamePasswords(true);
               }
+              createUserWithEmailAndPassword(auth, email, inputValue)
+                .then((userCredential) => {
+                  // Signed in
+                  const user = userCredential.user;
+                  // ...
+                })
+                .catch((error) => {
+                  const errorCode = error.code;
+                  const errorMessage = error.message;
+                  // ..
+                });
             }}
           >
             Register
